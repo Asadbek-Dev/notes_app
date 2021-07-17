8a80175cfd61340ab6ce89e69c2a7d8d82afeb70
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-07-17 17:58:54 +0500

    added sass and form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './App.scss'
 
 function App() {
 
+  const addNote=()=>{
+
+  };
+
   return (
-    <>
-      
-    </>
+    <div className='app'>
+      <h1>Sticky Notes</h1>
+      <form onSubmit={addNote} className='note-form'>
+        <textarea placeholder='Create a new note...'></textarea>
+        <button>Add</button>
+      </form>
+    </div>
   );
 }
 
