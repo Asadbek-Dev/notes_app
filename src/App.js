1b51b920bc66efb06a9175fc660fb1cf95ec3eec
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-07-18 00:30:46 +0500

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import './App.scss'
+import React, { useState, useReducer } from 'react';
+
+const initialNotesState={
+  lastNoteCreated:null,
+  totalNotes:0,
+  notes:[]
+};
+
+
+const notesReducer=(prevstate,action)=>{
+  switch(action.type){
+    case 'ADD_NOTE':{
+      const newState = {
+          lastNoteCreated: new Date().toTimeString().slice(0,8),
+          totalNotes:prevstate.notes.length + 1,
+          notes:[...prevstate.notes,action.payload]
+      };
+
+      console.log('After ADD_NOTE: ',newState);
+      return newState;
+    }
+  }
+};
 
 function App() {
+  const [noteInput, setNoteInput] = useState('');
+  const [notesState,dispatch]=useReducer(notesReducer,initialNotesState)
 
-  const addNote=()=>{
-
+  const addNote= event =>{
+    event.preventDefault();
+    if(!noteInput){
+      return ;
+    }
+    const newNote={
+      text:noteInput,
+    }
+    dispatch({ type:'ADD_NOTE',payload:newNote})
   };
-
   return (
     <div className='app'>
       <h1>Sticky Notes</h1>
       <form onSubmit={addNote} className='note-form'>
-        <textarea placeholder='Create a new note...'></textarea>
+        <textarea value={noteInput} onChange={event => setNoteInput(event.target.value)} placeholder='Create a new note...'></textarea>
         <button>Add</button>
       </form>
+      {noteInput}
     </div>
   );
 }
